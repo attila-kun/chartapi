1ba4a221bd28edfde131931d3b6863862462f347
Attila Kun <hello@DESKTOP-8AGJOHC.localdomain>
2021-02-04 21:09:51 +0000

    Using token from environment to make request.

--- iex/src/lib.rs
+++ iex/src/lib.rs
@@ -1,11 +1,13 @@
 use actix_web::client::Client;
 use actix_rt;
+use url;
 
-pub async fn make_request() -> String {
+pub async fn make_request(url: &str) -> String {
     let mut client = Client::default();
 
     // Create request builder and send request
-    let response = client.get("http://www.rust-lang.org")
+
+    let response = client.get(url)
        .header("User-Agent", "actix-web/3.0")
        .send()     // <- Send request
        .await;     // <- Wait for response
@@ -15,6 +17,9 @@ pub async fn make_request() -> String {
 
 #[actix_rt::test]
 pub async fn make_request_test() {
-    let body = make_request().await;
-    println!("{}", body);
+    let IEX_TOKEN = std::env::var("IEX_TOKEN").unwrap();
+    let mut url = url::Url::parse("https://sandbox.iexapis.com/stable/stock/twtr/intraday-prices").unwrap();
+    url.query_pairs_mut().append_pair("token", &IEX_TOKEN);
+    let body = make_request(&url.to_string()).await;
+    println!("body is: {}", body);
 }
