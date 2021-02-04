b3767ce5962738a38214cf79b2cff98a8e032cf5
Attila Kun <>
2021-02-04 22:04:12 +0000

    Added lazy_async to iex.

--- iex/src/lib.rs
+++ iex/src/lib.rs
@@ -1,11 +1,19 @@
 use actix_web::client::Client;
 use actix_rt;
+use lazy_static::{lazy_static};
 use url;
 
-async fn make_request(url: &str) -> String {
-    let mut client = Client::default();
+lazy_static! {
+    static ref IEX_TOKEN: String = std::env::var("IEX_TOKEN").unwrap();
+}
 
-    let response = client.get(url)
+async fn make_request(url_base: &str) -> String {
+
+    let mut url = url::Url::parse(url_base).unwrap();
+    url.query_pairs_mut().append_pair("token", &IEX_TOKEN);
+
+    let mut client = Client::default();
+    let response = client.get(url.to_string())
        .header("User-Agent", "actix-web/3.3.2")
        .send()
        .await;
@@ -13,16 +21,13 @@ async fn make_request(url: &str) -> String {
     String::from_utf8(response.unwrap().body().await.unwrap().to_vec()).unwrap()
 }
 
-async fn request_intraday_prices(symbol: &str, token: &str) -> String {
+async fn request_intraday_prices(symbol: &str) -> String {
     // TODO: make sandbox part of config
-    let mut url = url::Url::parse(&format!("https://sandbox.iexapis.com/stable/stock/{}/intraday-prices", symbol)).unwrap();
-    url.query_pairs_mut().append_pair("token", token);
-    make_request(&url.to_string()).await
+    make_request(&format!("https://sandbox.iexapis.com/stable/stock/{}/intraday-prices", symbol)).await
 }
 
 #[actix_rt::test]
 pub async fn make_request_test() {
-    let IEX_TOKEN = std::env::var("IEX_TOKEN").unwrap();
-    let body = request_intraday_prices("tsla", &IEX_TOKEN).await;
+    let body = request_intraday_prices("tsla").await;
     println!("body is: {}", body);
 }
