c6013793796b4e976aae852313d5435d63cf7784
Attila Kun <hello@DESKTOP-8AGJOHC.localdomain>
2021-02-04 21:31:18 +0000

    Request intraday prices.

--- iex/src/lib.rs
+++ iex/src/lib.rs
@@ -2,24 +2,27 @@ use actix_web::client::Client;
 use actix_rt;
 use url;
 
-pub async fn make_request(url: &str) -> String {
+async fn make_request(url: &str) -> String {
     let mut client = Client::default();
 
-    // Create request builder and send request
-
     let response = client.get(url)
-       .header("User-Agent", "actix-web/3.0")
-       .send()     // <- Send request
-       .await;     // <- Wait for response
+       .header("User-Agent", "actix-web/3.3.2")
+       .send()
+       .await;
 
     String::from_utf8(response.unwrap().body().await.unwrap().to_vec()).unwrap()
+}
+
+async fn request_intraday_prices(symbol: &str, token: &str) -> String {
+    // TODO: make sandbox part of config
+    let mut url = url::Url::parse(&format!("https://sandbox.iexapis.com/stable/stock/{}/intraday-prices", symbol)).unwrap();
+    url.query_pairs_mut().append_pair("token", token);
+    make_request(&url.to_string()).await
 }
 
 #[actix_rt::test]
 pub async fn make_request_test() {
     let IEX_TOKEN = std::env::var("IEX_TOKEN").unwrap();
-    let mut url = url::Url::parse("https://sandbox.iexapis.com/stable/stock/twtr/intraday-prices").unwrap();
-    url.query_pairs_mut().append_pair("token", &IEX_TOKEN);
-    let body = make_request(&url.to_string()).await;
+    let body = request_intraday_prices("tsla", &IEX_TOKEN).await;
     println!("body is: {}", body);
 }
